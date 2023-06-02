ed5cb8dedb90a3ad20ed16cc81070ffff235022e
GiselaLopes <giselalopes1999@gmail.com>
2023-06-02 20:01:38 -0300

    Ta pronto!

---
+++ Atividademod11.java
@@ -0,0 +1,40 @@
+/*
+ @autor Gisela
+ */
+
+
+import images.Nome;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Atividademod11 {
+
+     public static void main (String[] args) {
+         System.out.println("Nome e sexo biologico");
+         List<String> lista = new ArrayList<String>();
+         lista.add ("Gisela Feminino");
+         lista.add ("Everton masculino");
+         lista.add ("João Victor masculino");
+         lista.add ("Vanessa feminino");
+         System.out.println(lista);
+         //colection.sort(lista);
+
+
+
+         System.out.println("***Nome e sexo biologico***");
+         lista = new ArrayList<String>();
+         lista.add ("Everton masculino");
+         lista.add ("João Victor masculino");
+         System.out.println(lista);
+         //colection.sort(lista);
+
+
+         System.out.println("Nome e sexo biologico fm");
+         lista = new ArrayList<String>();
+         lista.add ("Gisela Feminino");
+         lista.add ("João Victor masculino");
+         System.out.println(lista);
+         //colection.sort(lista);
+     }
+}
